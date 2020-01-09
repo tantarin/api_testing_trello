ae27dbb424e606abc95999fa5f23879f6679caa9
tantarin <tanya_tarinskaya@mail.ru>
2020-01-09 12:36:06 +0300

    6

--- src/test/java/TrelloBoardTests.java
+++ src/test/java/TrelloBoardTests.java
@@ -12,10 +12,6 @@ import static api.BoardApi.successResponse;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-//https://api.trello.com/1/boards/?name=getid&key=cb9d5210ac7241d40bd7d4bb52fa2fc7&token=659afeb183ec0cc6c57a0f9bd70af5526d351e28a9c026e6a6cd1a213cc9438d
-//5e161f718b977127681979f5
-//5e162b5af3467a2a22cb6437
-//https://api.trello.com/1/boards/5e162f8d5639d52cf64a790e?fields=name,url&key=cb9d5210ac7241d40bd7d4bb52fa2fc7&token=659afeb183ec0cc6c57a0f9bd70af5526d351e28a9c026e6a6cd1a213cc9438d
 public class TrelloBoardTests {
 
     @Test
@@ -37,15 +33,16 @@ public class TrelloBoardTests {
     @Test
     public void deleteBoardTest() {
         Board answers =
-                BoardApi.getBoardAnswer(BoardApi.with().deleteBoard("5e162b5af3467a2a22cb6437"));
+                BoardApi.getBoardAnswer(BoardApi.with().deleteBoard("VWXoNTRa"));
         assertThat(answers.getName(), equalTo(null));
+        BoardApi.responseSpecification().expect().spec(successResponse());
     }
 
     @Test
     public void updateBoardTest() {
         Board answers =
-                BoardApi.getBoardAnswer(BoardApi.with().updateBoard("5e1463928e727580f94eeb75"));
-        assertThat(answers, equalTo("motherr"));
+                BoardApi.getBoardAnswer(BoardApi.with().updateBoard("zd3uRo5j"));
+        assertThat(answers.getName(), equalTo("9fd7ed6616441"));
     }
 }
 
